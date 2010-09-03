029931c71c6fbfcaef72988b90e98755adeda539
vowstar <vowstar@gmail.com>
2010-09-03 01:44:55 +0000

    release2

--- CommBug/AboutWindow.cs
+++ CommBug/AboutWindow.cs
@@ -5,19 +5,19 @@ namespace CommBug
 	{
 		protected virtual void OnButtonOkClicked (object sender, System.EventArgs e)
 		{
-			this.Destroy();
+			this.Destroy ();
 		}
-		
-		
+
+
 		public AboutWindow ()
 		{
 			this.Build ();
-			labelInfo.Text=labelInfo.Text.Replace("{VERSION}",System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());	
-			labelInfo.Text=labelInfo.Text.Replace("{YEAR}",DateTime.Now.Year.ToString());
-			labelInfo.Text=labelInfo.Text.Replace("{MONTH}",DateTime.Now.Month.ToString());
-			labelInfo.UseMarkup=true;
-			labelAuthor.UseMarkup=true;
-			labelTranslation.UseMarkup=true;
+			labelInfo.Text = labelInfo.Text.Replace ("{VERSION}", System.Reflection.Assembly.GetExecutingAssembly ().GetName ().Version.ToString ());
+			labelInfo.Text = labelInfo.Text.Replace ("{YEAR}", DateTime.Now.Year.ToString ());
+			labelInfo.Text = labelInfo.Text.Replace ("{MONTH}", DateTime.Now.Month.ToString ());
+			labelInfo.UseMarkup = true;
+			labelAuthor.UseMarkup = true;
+			labelTranslation.UseMarkup = true;
 		}
 	}
 }
